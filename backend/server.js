ad0198abe8702dbd5e479650200bf9f66bb2d0f2
Charisse Calma <ccbaekcha@gmail.com>
2023-10-27 17:25:40 -0700

    Redis forces itself to disconnect. Created a refresh token route, access token gets sent through cookies (http only), secure set to false, needs to be changed when creating prod env

--- backend/server.js
+++ backend/server.js
@@ -5,15 +5,38 @@ const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const { Schema } = require('mongoose')
 const redis = require('redis')
-
+const bodyParser = require('body-parser')
+const { _applyPlugins } = require('mongoose')
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan')
 const app = express()
-const client = redis.createClient()
+const client = redis.createClient();
 
 
-
+app.use(morgan('dev'));
+app.use(bodyParser.json())
+app.use(cookieParser());
 require('dotenv').config()
 app.use(express.json())
 app.use(cors())
+client.connect()
+
+client.on('connect', () => {
+    console.log('Connected to Redis');
+});
+
+client.on('ready', () => {
+    console.log('Redis client is ready for commands');
+});
+
+client.on('error', (err) => {
+    console.error('Redis client error:', err);
+});
+
+client.on('end', () => {
+    console.log('Redis client disconnected');
+});
+
 
 const connectDB = async() => {
     try{
@@ -37,42 +60,50 @@ const userSchema = mongoose.Schema({
     password: String
 })
 
-const userTokenSchema = mongoose.Schema({
-    userId: {
-        type: Schema.Types.ObjectId, 
-        require: true
-    },
-    token: {
-        type: String, 
-        required: true
-    },
-    createdAt:{
-        type: Date, 
-        default: Date.now,
-        expires: 30 * 84600
-    }
-})
-
 const User = mongoose.model("Users", userSchema)
-const UserToken = mongoose.model("UserToken", userTokenSchema)
+
 
 app.post('/login', async(req, res) => {
     const user = await User.findOne({ email: req.body.email });
+
     console.log('user', user);
-    if(!user) 
-        return res.status(400).send('Account does not exist.');
+    if(!user) return res.status(400).send('Account does not exist.');
 
     const verifiedPassword = await bcrypt.compare(req.body.password, user.password)
     if(!verifiedPassword)
         return res.send('Sorry, your password was incorrect.\nPlease double check your password.')
+    
+    const { accessToken, refreshToken } = generateAccessToken(user)
+    //client.set(refreshToken, user._id, 'EX', 7 * 24 * 60 * 60)
 
-    const {accessToken, refreshToken} = generateAccessToken(user)
-    res.status(200).json({
-            error: false,
-            accessToken,
-            refreshToken,
-            message: "Logged in sucessfully",
-        });
+    if (client.connected) {
+        client.set(refreshToken, user._id, 'EX', 7 * 24 * 60 * 60);
+    } else {
+        console.error("Redis client isn't connected");
+    }
+    
+
+    res.cookie('refreshToken', refreshToken, { httpOnly: true, secure: false })
+    res.cookie('accessToken', accessToken, { httpOnly: true, secure: false , maxAge: 10 * 60 * 1000})
+
+})
+
+app.post('/refresh-token', (req, res) => {
+    const refreshToken = req.cookies.refreshToken
+
+    jwt.verify(refreshToken, 'refresh_secret', (err, user) => {
+        if(err) return res.status(403).send('Invalid Refresh Token.');
+
+        client.get(refreshToken, (err, userId) => {
+            if (err || !userId || userId !== user._id) {
+                return res.status(403).send('Refresh Token is not valid');
+            }
+            const newAccessToken = jwt.sign({ id: userId }, 'access_secret', { expiresIn: '10m' });
+            res.cookie('accessToken', newAccessToken, { httpOnly: true, secure: false, maxAge: 10 * 60 * 1000})
+
+            res.send('Token refreshed successfully.')
+        })
+    })
 })
 
 app.post('/add-user', async(req, res) => {
@@ -95,16 +126,10 @@ app.post('/add-user', async(req, res) => {
 })
 
 const generateAccessToken = async(user) => {
-    const accessToken = jwt.sign(JSON.stringify(user), process.env.ACCESS_SECRET_TOKEN, { expiresIn: '15m'}) 
-    const refreshToken = jwt.sign(JSON.stringify(user), process.env.REFRESH_SECRET_TOKEN, { expiresIn: '30d'})
+    const accessToken = jwt.sign({id: JSON.stringify(user._id)}, 'access_secret', { expiresIn: '10m' }); 
+    const refreshToken = jwt.sign({id: JSON.stringify(user._id)}, 'refresh_secret', { expiresIn: '7d' });
 
-    const userToken = await UserToken.findOne({ userId: user._id})
-    if(userToken) await userToken.remove()
-
-    await new UserToken({ userId: user._id, token:refreshToken }).save()
-    res.cookie('refreshToken', refreshToken, { httpOnly: true, sameSite: 'strict'})
-    .header('Authorization', 'Bearer ' + accessToken)
-    .send(user);
+    return {accessToken, refreshToken}
 
 }
 
