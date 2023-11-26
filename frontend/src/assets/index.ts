58199576c849d106b71fe9f5eddc25218dd241e9
Charisse Calma <ccbaekcha@gmail.com>
2023-11-25 17:12:51 -0800

    Added designs for header, does not have functionality yet

--- frontend/src/assets/index.ts
+++ frontend/src/assets/index.ts
@@ -1,9 +1,13 @@
 import Logo from './images/logo-devlinks-large.svg';
 import emailSymbol from './images/icon-email.svg';
 import passwordSymbol from './images/icon-password.svg';
+import logoSymbol from './images/logo-devlinks-small.svg';
+import previewIcon from './images/icon-preview-header.svg';
 
 export {
     Logo,
     emailSymbol,
-    passwordSymbol
+    passwordSymbol,
+    logoSymbol,
+    previewIcon
 };
